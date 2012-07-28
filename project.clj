43a016664ab70ea5e0564922ceb9059b15035cbe
Ryan Crum <ryan.j.crum@gmail.com>
2012-07-28 14:04:35 -0400

    Bump version to 1.0.2

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject tailor "1.0.1-SNAPSHOT"
+(defproject tailor "1.0.2-SNAPSHOT"
   :description "Clojure library for creating files in the unified diff format. Not a file-differencing library."
   :dependencies [[org.clojure/clojure "1.2.1"]])
